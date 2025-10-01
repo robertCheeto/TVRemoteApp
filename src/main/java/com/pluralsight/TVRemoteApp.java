c168259bc766d0f9750686fa459503dd01c3844c
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-01 15:09:48 -0400

    * first commit, initialized main(), imported and created Scanner

---
+++ src/main/java/com/pluralsight/TVRemoteApp.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class TVRemoteApp {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
